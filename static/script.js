2cb2b4cfc0167075e6410a24fddd9ad892a77fab
tri10 <shubhi.blog@gmail.com>
2021-10-26 19:42:33 +0530

    yay2

--- static/script.js
+++ static/script.js
@@ -7,8 +7,8 @@ window.addEventListener('load', () => {
 
 
     function onResize() {
-        canvas.width = window.innerWidth / 2;
-        canvas.height = window.innerHeight / 2;
+        canvas.width = window.innerWidth/2 ;
+        canvas.height = window.innerHeight/2;
     }
     onResize();
 
@@ -22,6 +22,7 @@ window.addEventListener('load', () => {
     function startDraw() {
         drawing = true;
         console.log('starting ')
+        cDraw()
 
     }
     function endDraw() {
@@ -41,6 +42,19 @@ window.addEventListener('load', () => {
 
 
     }
+    const draw = function(x, y){
+        
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.lineTo(x, y)
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(x, y)
+
+        
+        // ctx.closePath();
+      };
+    
     function onSave() {
         var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
         fetch("predict",
@@ -60,9 +74,30 @@ window.addEventListener('load', () => {
             .catch((res) => { console.log(res) })
         
     }
+    
+      
     canvas.addEventListener('mousedown', startDraw)
+    // canvas.addEventListener('touchstart', handleStart,false)
     canvas.addEventListener('mouseup', endDraw)
+    // canvas.addEventListener('touchend', handlerEnd,false)
     canvas.addEventListener('mousemove', cDraw)
+    canvas.addEventListener('touchmove', function(e){
+        e.preventDefault();
+
+        for(let touches of e.changedTouches){
+
+            draw(touches.pageX,touches.pageY);
+        }
+        // draw(e.changedTouches[1].pageX, e.changedTouches[1].pageY);
+      });
+      canvas.addEventListener('touchend', function(e){
+        e.preventDefault();
+
+        ctx.beginPath();
+        // draw(e.changedTouches[1].pageX, e.changedTouches[1].pageY);
+      });
+      
+    
 
 
 
